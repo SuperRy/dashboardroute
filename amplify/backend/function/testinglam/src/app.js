393421516e8c38ef500c041c060d4704491b17b3
SuperRy <44445730+SuperRy@users.noreply.github.com>
2021-09-01 11:13:49 -0400

    test commit

---
+++ amplify/backend/function/testinglam/src/app.js
@@ -0,0 +1,100 @@
+/*
+Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+    http://aws.amazon.com/apache2.0/
+or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+*/
+
+
+
+const AWS = require('aws-sdk')
+
+var express = require('express')
+var bodyParser = require('body-parser')
+var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+
+// declare a new express app
+var app = express()
+app.use(bodyParser.json())
+app.use(awsServerlessExpressMiddleware.eventContext())
+
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+let tableName = "data2database";
+
+// Enable CORS for all methods
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "*")
+  next()
+});
+
+
+
+const userIdPresent = true; // TODO: update in case is required to use that definition
+const partitionKeyName = "spot1";
+const partitionKeyType = "S";
+const path = "/data2";
+const UNAUTH = 'UNAUTH';
+const hashKeyPath = '/:' + partitionKeyName;
+/**********************
+ * Example get method *
+ **********************/
+
+
+
+const convertUrlType = (param, type) => {
+  switch(type) {
+    case "N":
+      return Number.parseInt(param);
+    default:
+      return param;
+  }
+}
+
+
+app.get(path + hashKeyPath, function(req, res) {
+  var condition = {}
+  condition[partitionKeyName] = {
+    ComparisonOperator: 'EQ'
+  }
+
+  if (userIdPresent && req.apiGateway) {
+    condition[partitionKeyName]['AttributeValueList'] = [req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH ];
+  } else {
+    try {
+      condition[partitionKeyName]['AttributeValueList'] = [ convertUrlType(req.params[partitionKeyName], partitionKeyType) ];
+    } catch(err) {
+      res.statusCode = 500;
+      res.json({error: 'Wrong column type ' + err});
+    }
+  }
+
+  let queryParams = {
+    TableName: tableName,
+    KeyConditions: condition
+  }
+
+  dynamodb.scan(queryParams, (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.json({error: 'Could not load items: ' + err});
+    } else {
+      res.json(data.Items);
+    }
+  });
+});
+
+
+
+
+app.listen(3000, function() {
+    console.log("App started")
+});
+
+// Export the app object. When executing the application local this does nothing. However,
+// to port it to AWS Lambda we will create a wrapper around that will load the app from
+// this file
+module.exports = app
--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import Amplify, {API, Auth} from 'aws-amplify'
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react'
+//import { configure } from '@testing-library/react';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig)
 
 function App() {
+
+  useEffect(() => {
+    //API.get('dashboardAPI',"/data1/Key").then((projectdata) => console.log(projectdata));
+    API.get('dashboardAPI',"/data2/spot1").then((projectdata1) => console.log(projectdata1));
+    API.get('dashboardAPI',"/data3/first").then((projectdata2) => console.log(projectdata2));
+  },[]);
+
+
   return (
-    <div className="App">
+    <div className="App" >
+      <AmplifySignOut buttonText="Leave the System"/>
       <header className="App-header">
+        <h3>Testing upload</h3>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p cla> </p>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
